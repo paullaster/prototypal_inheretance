9756db3b44e20785fd6132671ddfb43e98109a89
paullaster <paullasterokoth98@gmail.com>
2022-09-08 11:20:14 +0300

    implimenting js class static functions

--- class_inheritance.js
+++ class_inheritance.js
@@ -7,16 +7,19 @@ class Users{
         this.password = password;
     }
     getUserDetails(){
-        return `username:${this.username}, password: ${this.password}`;
+        return `
+        username:${this.username}\n
+        password: ${this.password}`;
     }
 }
 
 
 class Student extends Users{
-    constructor(username, password, email, reg){
+    constructor(username, password, email, reg, course){
         super(username,password);
         this.email = email;
         this.reg = reg;
+        this.course = course;
         this.totalFee = 450
         this.feePaid = null;
         this.feeBal = null;
@@ -31,13 +34,26 @@ class Student extends Users{
     checkFeeBal(){
         //this.feePaid = this.payFee(amount);
         this.feeBal = this.totalFee - this.feePaid;
-        console.log(`${this.username}\`s  fee balance is : $${this.feeBal}`);
+       // console.log(`${this.username}\`s  fee balance is : $${this.feeBal}`);
         return this.feeBal;
     }
     studentRecords(){
         const userdetails = super.getUserDetails();
-        console.log(`${userdetails}, email: ${this.email},
-         registration number: ${this.reg}  Fee Balance: $${this.feeBal}`);
+        console.log(`
+        ${userdetails}\n
+        email: ${this.email}\n
+        registration number: ${this.reg}\n
+        Fee Balance: $${this.feeBal}\n
+        Course: ${this.course} `);
+    }
+    static fetUsers(...std){
+        for(let s of std){
+            console.log(`
+                Username: ${s.username}\n
+                Course: ${s.course}\n
+                Fee Balance: ${s.feeBal}`);
+        }
+
     }
 }
 
@@ -48,7 +64,16 @@ cee.getUserDetails();
 
 
 //Creating new student
-let std1 = new Student('Cynthia', 'Cee@123$', 'cee@school.io', 'sst-123-001/2019');
-std1.payFee(250);
+let std1 = new Student('Cynthia', 'Cee@123$', 'cee@school.io', 'sst-123-001/2019', 'Tourism management');
+let std2 = new Student('Fred', '@123Fredo$', 'fred@school.io', 'cit-123-001/2019', 'Hospitality management');
+let std3 = new Student('Laster', 'thee@1&23$', 'laster@school.io', 'cit-221-057/2017', 'Computer Science');
+std1.payFee(1050);
 std1.checkFeeBal();
-std1.studentRecords();
+
+std2.payFee(80);
+std2.checkFeeBal();
+
+std3.payFee(20);
+std3.checkFeeBal();
+//std1.studentRecords();
+Student.fetUsers(std1, std2, std3);
