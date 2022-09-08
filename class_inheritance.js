3593ca8e6b167d8f3bb0f3d09636f35831fdb562
paullaster <paullasterokoth98@gmail.com>
2022-09-08 08:49:16 +0300

    classical inheritance

--- class_inheritance.js
+++ class_inheritance.js
@@ -7,14 +7,45 @@ class Users{
         this.password = password;
     }
     getUserDetails(){
-        console.log(`username:${this.username}, password: ${this.password}`);
+        return `username:${this.username}, password: ${this.password}`;
     }
 }
 
 
-
+class Student extends Users{
+    constructor(username, password, email, reg){
+        super(username,password);
+        this.email = email;
+        this.reg = reg;
+        this.totalFee = 450
+        this.feePaid = null;
+        this.feeBal = null;
+    }
+    registerStudent(){
+       return this.feeBal = this.totalFee
+    }
+    studentRecords(){
+        const userdetails = super.getUserDetails();
+        this.feeBal = this.registerStudent();
+        console.log(`${userdetails}, email: ${this.email},
+         registration number: ${this.reg}  Fee Balance: $${this.feeBal}`);
+    }
+    checkFeeBal(){
+        console.log(`${this.username}\`s  fee balance is : $${this.feeBal}`);
+    }
+    payFee(amount){
+        this.feePaid = amount;
+        this.feeBal = this.totalFee - this.feePaid;
+    }
+}
 
 //Creating a new User
 
 let cee = new Users('Cee', '@#$%');
 cee.getUserDetails();
+
+
+//Creating new student
+let std1 = new Student('Cynthia', 'Cee@123$', 'cee@school.io', 'sst-123-001/2019');
+std1.studentRecords();
+std1.checkFeeBal();
